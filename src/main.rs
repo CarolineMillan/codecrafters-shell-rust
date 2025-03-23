0fd69670d0b7b592bbe424331b587c15b60d6c42
caroline <carolinemillan5678@gmail.com>
2025-03-23 20:28:41 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use pathsearch::find_executable_in_path;
-use std::{env::{self, join_paths, split_paths}, io::{self, Write}, ops::Deref, path::PathBuf, str::SplitWhitespace};
+use std::{env::{self, join_paths, split_paths}, io::{self, Write}, ops::Deref, path::PathBuf, process::Command, str::SplitWhitespace};
 use std::process::exit;
 
 /*
@@ -20,7 +20,7 @@ and execute methods (called in parse match statement)
 the main func will be a fde loop basically
 */
 
-pub struct Command<'a> {
+pub struct MyCommand<'a> {
     // head is the command and tail is the arguments (maybe rename the fields..)
     head: Option<&'a str>,
     tail: Vec<&'a str>,
@@ -29,14 +29,12 @@ pub struct Command<'a> {
 // valid commands
 const CMDS: [&str; 3] = ["exit", "echo", "type"];
 
-impl<'a> Command<'a> {
+impl<'a> MyCommand<'a> {
     fn new(input: &'a str) -> Self {
-        let mut command = input.split_whitespace();
+        let mut my_command = input.split_whitespace();
 
-        // I'm not sure about using this, purely because of exit 0 
-        // I'm not sure if the 0 is necessary 
-        let head = command.next();
-        let tail = command.collect::<Vec<&str>>();//.join(" ");
+        let head = my_command.next();
+        let tail = my_command.collect::<Vec<&str>>();//.join(" ");
 
         Self {
             head,
@@ -45,27 +43,33 @@ impl<'a> Command<'a> {
     }
 }
 
-fn parse(command: Command) {
-    match command.head.unwrap() {
+fn parse(my_command: MyCommand) {
+    match my_command.head.unwrap() {
         // exit w code 0
-        "exit" => exit(command.tail[0].parse().unwrap()),
-        "echo" => println!("{}", command.tail.join(" ")),
+        "exit" => exit(my_command.tail[0].parse().unwrap()),
+        "echo" => println!("{}", my_command.tail.join(" ")),
         "type" => {
-            for arg in command.tail.clone().into_iter() {
+            for arg in my_command.tail.clone().into_iter() {
                 if CMDS.contains(&arg) {
                     println!("{} is a shell builtin", arg);
                 } 
-                else {
-                    if let Some(path) = find_executable_in_path(arg) {
+                else if let Some(path) = find_executable_in_path(arg) {
                     println!("{} is {}", arg, path.to_str().unwrap());
-                    } 
-                    else {
-                        println!("{} not found", command.tail.join(" "));
-                    }
+                } 
+                else {
+                    println!("{} not found", my_command.tail.join(" "));
                 }
             }
         }
-        _ => println!("{}: command not found", command.head.unwrap())
+        // need another match for an executable file name
+        // that will execute the file and pass in the arguments provided
+        _ => {
+            if let Some(path) = find_executable_in_path(my_command.head.clone().unwrap()) {
+                let _output = Command::new(path).args(my_command.tail).output().expect("failed to execute file");
+            }
+            println!("{}: command not found", my_command.head.unwrap())
+        
+        }
     }
 }
 
@@ -82,23 +86,12 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         // read the command
-        let my_command = Command::new(&input.trim());
+        let my_command = MyCommand::new(&input.trim());
 
         // parse the command
         parse(my_command);
         
     }
-}
-
-fn get_path(args: Vec<&str>) -> Result<(), env::JoinPathsError> {
-    if let Some(path) = env::var_os("PATH") {
-        let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-        paths.push(PathBuf::from("/home/xyz/bin"));
-        let new_path = env::join_paths(paths)?;
-        env::set_var("PATH", &new_path);
-    }
-
-    Ok(())
 }
 
 /*
