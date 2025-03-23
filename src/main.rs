2f91717903be357cce1d37cad6f746c147314cf6
caroline <carolinemillan5678@gmail.com>
2025-03-23 15:26:50 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use pathsearch::find_executable_in_path;
+use std::{io::{self, Write}, ops::Deref, str::SplitWhitespace};
+use std::process::exit;
+
+/*
+
+This is like the chip-8 interpreter
+the input string is like an opcode (create a struct becuase it has a structure!!)
+which we then parse ("decode") using a match statement
+and execute usign other hlper functions
+
+so we want a command struct
+a fetch method (creates an instance of command struct using input)
+
+a parse method (match statement)
+
+and execute methods (called in parse match statement)
+
+the main func will be a fde loop basically
+*/
+
+pub struct Command<'a> {
+    // head is the command and tail is the arguments (maybe rename the fields..)
+    head: Option<&'a str>,
+    tail: Vec<&'a str>,
+}
+
+// valid commands
+const CMDS: [&str; 3] = ["exit", "echo", "type"];
+
+impl<'a> Command<'a> {
+    fn new(input: &'a str) -> Self {
+        let mut command = input.split_whitespace();
+
+        // I'm not sure about using this, purely because of exit 0 
+        // I'm not sure if the 0 is necessary 
+        let head = command.next();
+        let tail = command.collect::<Vec<&str>>();//.join(" ");
+
+        Self {
+            head,
+            tail,
+        }
+    }
+}
+
+fn parse(command: Command) {
+    match command.head.unwrap() {
+        // exit w code 0
+        "exit" => exit(command.tail[0].parse().unwrap()),
+        "echo" => println!("{}", command.tail.join(" ")),
+        "type" => {
+            for arg in command.tail {
+                if CMDS.contains(&arg) {
+                    println!("{} is a shell builtin", arg);
+                } else if let Some(path) = find_executable_in_path(arg) {
+                    println!("{} is {}", arg, path.to_str().unwrap());
+                } else {
+                    println!("{} not found", arg);
+                }
+            }
+        }
+        _ => println!("{}: command not found", command.tail.join(" "))
+    }
+}
+
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -12,21 +77,66 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
+        // read the command
+        let my_command = Command::new(&input.trim());
+
+        // parse the command
+        parse(my_command);
+        
+    }
+}
+
+/*
+
+fn main() {
+    // Uncomment this block to pass the first stage
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        /*
+
         let mut command = input.split_whitespace();
 
+        // I'm not sure about using this, purely because of exit 0 
+        // I'm not sure if the 0 is necessary 
         let head = command.next();
         let tail = command.collect::<Vec<&str>>().join(" ");
-
+*/
         match head.unwrap() {
             "exit" => break,
+            "exit 0" => break,
             "echo" => println!("{}", tail),
             "type" => {
                 match tail.trim() {
-                    "exit 0" | "exit" | "echo" | "type" => println!("{} is a shell builtin", tail),
-                    _ => println!("{}: not found", tail),
+                    "exit" | "exit 0" | "echo" | "type" => println!("{} is a shell builtin", tail),
+                    //search for tail in path and print path, else print invalid command
+                    _ => {
+                        // if valid path
+                        // i don't know how to test for a valid path
+                        /// what's the structure?
+                        /// oh no
+                        /// is this some kind of search algorithm
+                        /// i think it is
+                        /// i don't want to learn those yet!
+                        /// use .split_paths
+                        /// 
+                        if let Some(path) = find_executable_in_path(arg) {
+                            println!("{} is {}", arg, path.to_str().unwrap());
+                        }
+                        else {
+                        // else
+                        println!("{}: not found", tail)
+                        }
+                    },
                 }
             }
             _ => println!("{}: command not found", input.trim())
         }
     }
 }
+*/
