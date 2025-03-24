6d045e8ceaed985dfc0932ab4dbc66cc4a277263
caroline <carolinemillan5678@gmail.com>
2025-03-24 11:27:58 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -74,14 +74,20 @@ fn parse(my_command: MyCommand) -> Result<(), Box<dyn std::error::Error>> {
             // if it's ./dir, then what?
 
             // get a handle on input path
-            let path = Path::new(my_command.tail[0]).canonicalize().expect(format!("cd: {}: No such file or directory", my_command.tail[0]).as_ref());
+            let path = Path::new(my_command.tail[0]).canonicalize();//.expect(format!("cd: {}: No such file or directory", my_command.tail[0]).as_ref());
             
-            // deal with this after changes
-            //let res = set_current_dir(my_command.tail[0]);//.expect(format!("cd: {}: No such file or directory", my_command.tail[0]).as_ref());
-            let res = set_current_dir(path);
-
-            if res.is_err() {
+            if path.is_err() {
                 println!("cd: {}: No such file or directory", my_command.tail[0]);
+            }
+            else {
+                let res = set_current_dir(path.unwrap());
+            
+                // deal with this after changes
+                //let res = set_current_dir(my_command.tail[0]);//.expect(format!("cd: {}: No such file or directory", my_command.tail[0]).as_ref());
+                
+                if res.is_err() {
+                    println!("cd: {}: No such file or directory", my_command.tail[0]);
+                }
             }
         }
         _ => {
