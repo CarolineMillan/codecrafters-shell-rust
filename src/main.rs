82834a521b576350ba6841043e10b4083f2a4e41
caroline <carolinemillan5678@gmail.com>
2025-03-25 20:28:54 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -258,8 +258,9 @@ fn decode(my_command: MyCommand) -> Result<(), Box<dyn std::error::Error>> {
 fn main() {
     // Uncomment this block to pass the first stage
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
+        print_prompt().unwrap();
+        //print!("$ ");
+        //io::stdout().flush().unwrap();
 
         // Wait for user input
         let stdin = io::stdin();
@@ -273,6 +274,14 @@ fn main() {
         let _res = decode(my_command);
         
     }
+}
+
+fn print_prompt() -> io::Result<()> {
+    // Open /dev/tty to write directly to the terminal.
+    let mut tty = OpenOptions::new().write(true).open("/dev/tty")?;
+    write!(tty, "$ ")?;
+    tty.flush()?;
+    Ok(())
 }
 
 fn change_directory(dir: &str) {
