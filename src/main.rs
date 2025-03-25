7c99a9adc2e071849b0568c8c5334617809ffea1
caroline <carolinemillan5678@gmail.com>
2025-03-25 21:00:55 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -260,7 +260,7 @@ fn main() {
     loop {
         //print_prompt().expect("couldn't print $");
         print!("$ ");
-        std::io::stdout().flush().unwrap();
+        io::stdout().flush().unwrap();
         //output
         //let mut stdout_file: Box<dyn Write> = Box::new(io::stdout());
 
@@ -271,7 +271,7 @@ fn main() {
         
 
         // Wait for user input
-        let stdin = std::io::stdin();
+        let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
