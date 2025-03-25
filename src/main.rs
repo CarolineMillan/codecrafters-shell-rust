79cb0890d2292bede8fd73f1f21e49b75f79dec0
caroline <carolinemillan5678@gmail.com>
2025-03-25 20:30:52 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -258,9 +258,9 @@ fn decode(my_command: MyCommand) -> Result<(), Box<dyn std::error::Error>> {
 fn main() {
     // Uncomment this block to pass the first stage
     loop {
-        print_prompt().unwrap();
-        //print!("$ ");
-        //io::stdout().flush().unwrap();
+        //print_prompt().unwrap();
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
         // Wait for user input
         let stdin = io::stdin();
